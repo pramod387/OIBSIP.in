c3ba00035f3d6622dba87755e1d2b644fe582b06
Pramod Kumar Sah <105236159+pramod387@users.noreply.github.com>
2023-02-19 23:07:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// typing animation 
+var typed = new typed(".typing", {
+    strings: ["", "Student", "Web Designer", "Youtuber", "Programmer",],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
